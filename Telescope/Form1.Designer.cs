b909424644d149cd989e4a695011ec97565daeea
Filip <32719383+FilipHojgaard@users.noreply.github.com>
2017-11-01 16:15:46 +0100

    Første version af report implementeret

--- Telescope/Form1.Designer.cs
+++ Telescope/Form1.Designer.cs
@@ -49,14 +49,22 @@
             this.mUG = new System.Windows.Forms.Label();
             this.lUE = new System.Windows.Forms.Label();
             this.button3 = new System.Windows.Forms.Button();
+            this.reportBox = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
             ((System.ComponentModel.ISupportInitialize)(this.apertureUpDown)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.focalLengthUpDown)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(9, 18);
+            this.label1.Location = new System.Drawing.Point(3, 27);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(50, 13);
             this.label1.TabIndex = 0;
@@ -65,7 +73,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(92, 18);
+            this.label2.Location = new System.Drawing.Point(86, 27);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(72, 13);
             this.label2.TabIndex = 1;
@@ -74,7 +82,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(180, 18);
+            this.label3.Location = new System.Drawing.Point(174, 27);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(54, 13);
             this.label3.TabIndex = 2;
@@ -87,7 +95,7 @@
             0,
             0,
             0});
-            this.apertureUpDown.Location = new System.Drawing.Point(12, 34);
+            this.apertureUpDown.Location = new System.Drawing.Point(6, 43);
             this.apertureUpDown.Maximum = new decimal(new int[] {
             1000,
             0,
@@ -104,7 +112,7 @@
             0,
             0,
             0});
-            this.focalLengthUpDown.Location = new System.Drawing.Point(95, 34);
+            this.focalLengthUpDown.Location = new System.Drawing.Point(89, 43);
             this.focalLengthUpDown.Maximum = new decimal(new int[] {
             10000,
             0,
@@ -128,7 +136,7 @@
             "26 mm",
             "32 mm",
             "40 mm"});
-            this.eyePieceSelector.Location = new System.Drawing.Point(183, 32);
+            this.eyePieceSelector.Location = new System.Drawing.Point(177, 41);
             this.eyePieceSelector.Name = "eyePieceSelector";
             this.eyePieceSelector.Size = new System.Drawing.Size(96, 21);
             this.eyePieceSelector.TabIndex = 5;
@@ -136,7 +144,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(367, 21);
+            this.button1.Location = new System.Drawing.Point(361, 30);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(88, 41);
             this.button1.TabIndex = 6;
@@ -147,7 +155,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(12, 95);
+            this.label4.Location = new System.Drawing.Point(6, 24);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(44, 13);
             this.label4.TabIndex = 7;
@@ -156,7 +164,7 @@
             // fRatio
             // 
             this.fRatio.AutoSize = true;
-            this.fRatio.Location = new System.Drawing.Point(108, 95);
+            this.fRatio.Location = new System.Drawing.Point(102, 24);
             this.fRatio.Name = "fRatio";
             this.fRatio.Size = new System.Drawing.Size(15, 13);
             this.fRatio.TabIndex = 8;
@@ -165,7 +173,7 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(12, 119);
+            this.label6.Location = new System.Drawing.Point(6, 48);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(73, 13);
             this.label6.TabIndex = 9;
@@ -174,7 +182,7 @@
             // magnification
             // 
             this.magnification.AutoSize = true;
-            this.magnification.Location = new System.Drawing.Point(108, 119);
+            this.magnification.Location = new System.Drawing.Point(102, 48);
             this.magnification.Name = "magnification";
             this.magnification.Size = new System.Drawing.Size(14, 13);
             this.magnification.TabIndex = 10;
@@ -183,7 +191,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(293, 17);
+            this.label5.Location = new System.Drawing.Point(287, 26);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(42, 13);
             this.label5.TabIndex = 11;
@@ -198,7 +206,7 @@
             "2x",
             "2,3x",
             "3x"});
-            this.barlowSelector.Location = new System.Drawing.Point(296, 32);
+            this.barlowSelector.Location = new System.Drawing.Point(290, 41);
             this.barlowSelector.Name = "barlowSelector";
             this.barlowSelector.Size = new System.Drawing.Size(56, 21);
             this.barlowSelector.TabIndex = 12;
@@ -207,7 +215,7 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(12, 145);
+            this.label7.Location = new System.Drawing.Point(6, 74);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(60, 13);
             this.label7.TabIndex = 13;
@@ -216,7 +224,7 @@
             // resolution
             // 
             this.resolution.AutoSize = true;
-            this.resolution.Location = new System.Drawing.Point(108, 145);
+            this.resolution.Location = new System.Drawing.Point(102, 74);
             this.resolution.Name = "resolution";
             this.resolution.Size = new System.Drawing.Size(68, 13);
             this.resolution.TabIndex = 14;
@@ -224,7 +232,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(194, 141);
+            this.button2.Location = new System.Drawing.Point(188, 70);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(20, 21);
             this.button2.TabIndex = 15;
@@ -235,7 +243,7 @@
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(219, 95);
+            this.label8.Location = new System.Drawing.Point(213, 24);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(150, 13);
             this.label8.TabIndex = 16;
@@ -244,7 +252,7 @@
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(219, 119);
+            this.label9.Location = new System.Drawing.Point(213, 48);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(125, 13);
             this.label9.TabIndex = 17;
@@ -253,7 +261,7 @@
             // mUG
             // 
             this.mUG.AutoSize = true;
-            this.mUG.Location = new System.Drawing.Point(376, 95);
+            this.mUG.Location = new System.Drawing.Point(370, 24);
             this.mUG.Name = "mUG";
             this.mUG.Size = new System.Drawing.Size(13, 13);
             this.mUG.TabIndex = 18;
@@ -262,7 +270,7 @@
             // lUE
             // 
             this.lUE.AutoSize = true;
-            this.lUE.Location = new System.Drawing.Point(376, 119);
+            this.lUE.Location = new System.Drawing.Point(370, 48);
             this.lUE.Name = "lUE";
             this.lUE.Size = new System.Drawing.Size(23, 13);
             this.lUE.TabIndex = 19;
@@ -270,7 +278,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(435, 114);
+            this.button3.Location = new System.Drawing.Point(429, 43);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(20, 23);
             this.button3.TabIndex = 20;
@@ -278,39 +286,95 @@
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
+            // reportBox
+            // 
+            this.reportBox.Location = new System.Drawing.Point(18, 19);
+            this.reportBox.Multiline = true;
+            this.reportBox.Name = "reportBox";
+            this.reportBox.ReadOnly = true;
+            this.reportBox.Size = new System.Drawing.Size(431, 150);
+            this.reportBox.TabIndex = 21;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(15, 171);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(42, 13);
+            this.label10.TabIndex = 22;
+            this.label10.Text = "Report:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.apertureUpDown);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.focalLengthUpDown);
+            this.groupBox1.Controls.Add(this.eyePieceSelector);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.barlowSelector);
+            this.groupBox1.Location = new System.Drawing.Point(12, 6);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(462, 78);
+            this.groupBox1.TabIndex = 23;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Input";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.fRatio);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.magnification);
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.lUE);
+            this.groupBox2.Controls.Add(this.resolution);
+            this.groupBox2.Controls.Add(this.mUG);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.label9);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Location = new System.Drawing.Point(12, 100);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(462, 107);
+            this.groupBox2.TabIndex = 24;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Specifications";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.reportBox);
+            this.groupBox3.Location = new System.Drawing.Point(12, 213);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(462, 182);
+            this.groupBox3.TabIndex = 25;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Report";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.ClientSize = new System.Drawing.Size(471, 352);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.lUE);
-            this.Controls.Add(this.mUG);
-            this.Controls.Add(this.label9);
-            this.Controls.Add(this.label8);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.resolution);
-            this.Controls.Add(this.label7);
-            this.Controls.Add(this.barlowSelector);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.magnification);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.fRatio);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.eyePieceSelector);
-            this.Controls.Add(this.focalLengthUpDown);
-            this.Controls.Add(this.apertureUpDown);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(493, 413);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label10);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Form1";
             this.Text = "Telescope Calculator";
             ((System.ComponentModel.ISupportInitialize)(this.apertureUpDown)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.focalLengthUpDown)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -339,6 +403,11 @@
         private System.Windows.Forms.Label mUG;
         private System.Windows.Forms.Label lUE;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox reportBox;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
     }
 }
 
--- Telescope/Form1.cs
+++ Telescope/Form1.cs
@@ -15,6 +15,11 @@ namespace Telescope
         public double eyePiece;
         public double mag;
         public double barlow = 1;
+        public int apertureClass;
+        public int fClass;
+        public double f;
+        public bool reflector;
+        public bool both = false;
 
         public Form1()
         {
@@ -30,7 +35,8 @@ namespace Telescope
                 MessageBox.Show("You can not Divide by 0","Error");
             else
             {
-                fRatio.Text = "f/" + Math.Round(focalLengthUpDown.Value / apertureUpDown.Value, 1);
+                f = Convert.ToDouble(Math.Round(focalLengthUpDown.Value / apertureUpDown.Value, 1));
+                fRatio.Text = "f/" + f;
 
                 // Magnification calculation
                 mag = (Convert.ToDouble(focalLengthUpDown.Value) / eyePiece * barlow);  // mag bliver magnification efter focallength, eyepiece og barlow værdier
@@ -46,6 +52,11 @@ namespace Telescope
                 mUG.Text = "" + 2.5 * Convert.ToDouble(apertureUpDown.Value) + "X";
                 // Longest useful eyepiece calculation
                 lUE.Text = "" + 7 * Math.Round(focalLengthUpDown.Value / apertureUpDown.Value, 1) + " mm";
+                // Udregn og print report
+                reportBox.Text = "";
+                both = false;
+                scopeClass();
+                report();
             }
         }
 
@@ -97,5 +108,109 @@ namespace Telescope
                 "Your eyes pupil is about 7 mm, so your eyepiece should have a focal length no larger " +
                 "than the width of your pupil, which gets smaller as you age.", "Help");
         }
+
+        private void scopeClass()
+        {
+            // apertureClass
+            if (apertureUpDown.Value < 80)
+                apertureClass = 1;
+            else if (apertureUpDown.Value < 130)
+                apertureClass = 2;
+            else if (apertureUpDown.Value < 170)
+                apertureClass = 3;
+            else if (apertureUpDown.Value < 250)
+                apertureClass = 4;
+            else if (apertureUpDown.Value >= 250)
+                apertureClass = 5;
+            // fClass
+            if (f <= 6.5)
+                fClass = 1;
+            else if (f <= 9.5)
+                fClass = 2;
+            else if (f > 9.5)
+                fClass = 3;
+        }
+
+        private void report()
+        {
+            // Størrelse
+            if (apertureClass == 1)
+                reportBox.Text += "This is a small telescope\r\n";
+            else if (apertureClass == 2)
+                reportBox.Text += "This is a medium/small telescope\r\n";
+            else if (apertureClass == 3)
+                reportBox.Text += "This is a medium telescope\r\n";
+            else if (apertureClass == 4)
+                reportBox.Text += "This is a large telescope\r\n";
+            else if (apertureClass == 5)
+                reportBox.Text += "This is a very large telescope\r\n";
+            // Type
+            if (apertureClass == 1)
+            {
+                reportBox.Text += "It is probably a refractor\r\n";
+                reflector = false;
+            }
+                
+            else if (apertureClass == 2)
+            {
+                if (fClass == 3)
+                {
+                    reportBox.Text += "It is a refractor\r\n";
+                    reflector = false;
+                }
+                else if (fClass == 1 || fClass == 2)
+                {
+                    reportBox.Text += "It is probably a reflector\r\n";
+                    reflector = true;
+                }
+            }
+            else if (apertureClass == 3)
+            {
+                if (fClass == 3)
+                {
+                    reportBox.Text += "It could be both a refractor and reflector\r\n";
+                    both = true;
+                }
+                else if (fClass == 1 || fClass == 2)
+                {
+                    reportBox.Text += "It is a reflector\r\n";
+                    reflector = true;
+                }
+            }
+            if (apertureClass == 4)
+            {
+                reportBox.Text += "It is a reflector\r\n";
+                reflector = true;
+            }
+            if (apertureClass == 5)
+            {
+                reportBox.Text += "It is a reflector\r\n";
+                reflector = true;
+            }
+
+            // Styrker
+            if (both == true)
+            {
+                if (fClass == 2)
+                    reportBox.Text += "A telescope with a medium f ratio is good at both deep sky observing along with moon and planets. Use a low power eyepiece for the moon and a high power for nebuaes and star clusters\r\n";
+                else if (fClass == 1)
+                    reportBox.Text += "A telescope with a low f ratio is good at deep sky observing. Use a high power eyepiece and look for nebuales, galaxies and star clusters\r\n";
+                else if (fClass == 3)
+                    reportBox.Text += "A telescope with a high f ratio is good at moon/planets observing. Use a low eyepiece and look for moon craters, Saturns rings or Jupiters moons";
+            }
+            else if (reflector && fClass != 3)
+            {
+                reportBox.Text += "A low f ratio reflector is good at observing deep sky objects with a high power eyepiece. look for nebulaes, galaxies or star clusters\r\n";
+            }
+            else if (reflector && fClass == 3)
+            {
+                reportBox.Text += "A high f ratio reflector is not so common. If your aparture is great you can use this for both deep sky observing and moon/planets observing. If not use this telescope for moon/planet observing\r\n";
+            }
+            else
+            {
+                reportBox.Text += "A high f ratio refractor is good at observing the moon and planets. Use a low power eyepiece to gain magnifications and view moon craters, Saturns rings or Jupiters moons\r\n";
+            }
+
+            }
     }
 }
